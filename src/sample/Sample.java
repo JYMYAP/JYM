7b4d7bdeddb5e8c3e07421b1f5a5ee5bd0d48c7a
User <User@User-PC>
2017-10-30 13:42:03 +0800

    Second Branch

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -16,6 +16,8 @@ public class Sample {
      */
     public static void main(String[] args) {
         System.out.println("Main");
+        System.out.println("Amazing");
+        System.out.println("Second Branch");
     }
     
 }
